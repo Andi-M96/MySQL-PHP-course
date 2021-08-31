1f03c84ceef4361d47cf8d8fbda231013b78ab26
Andi-M96 <83184180+Andi-M96@users.noreply.github.com>
2021-08-31 15:03:55 +0100

    first commit

---
+++ Chapter_01/01_03/globe_bank/public/staff/pages/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>Andi's PHP site</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../../stylesheets/staff.css" />
+  </head>
+    
+  <body>
+    <header> 
+      <h1> Andee staff area </h1>
+    </header>
+
+    <navigation>
+      <ul>
+        <li><a href="index.php"></a></li>
+      </ul>
+    </navigation>
+
+    <footer>
+      &copy; <?php echo date('Y'); ?> ANDO
+    </footer>
+
+  </body>
+</html>
